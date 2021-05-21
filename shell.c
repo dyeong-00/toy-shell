689d42fbccdc66c3675edd7e5ec25ce45766cb8e
dyeong-00 <sjungsu3427@gmail.com>
2021-05-22 04:00:03 +0900

    shell.c

--- shell.c
+++ shell.c
@@ -6,34 +6,80 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#include <time.h>
+
 #define MAX_LEN_LINE    100
+#define maxlen 100
+
+
+void welcome()
+{
+  struct tm *t;
+  time_t now;
+  now = time(NULL);
+  t = localtime(&now);
+
+  printf("\n -------------------------\n");
+  printf("|  welcome to sun shell!  |\n");
+  printf("|       %d.%02d.%02d.       |\n", t->tm_year+1900, t->tm_mon + 1, t->tm_mday);
+  printf(" -------------------------\n\n");
+}
+
+void shell_prompt()
+{
+  char uname[maxlen], dname[maxlen];
+  getlogin_r(uname, maxlen);
+  getcwd(dname, maxlen);
+  printf("\e[93m%s\x1b[0m in \e[94m%s\x1b[0m: ", uname, dname);
+}
+
+int builtin_commend(char *args[])
+{
+  if (strcmp(args[0],"exit")==0)
+  {
+    exit(1);
+  }
+
+  if (strcmp(args[0], "cd")==0)
+  {
+    chdir("/");
+    return 1;
+  }
+  return 0;
+}
 
 int main(void)
 {
-    char command[MAX_LEN_LINE];
-    char *args[] = {command, NULL};
-    int ret, status;
-    pid_t pid, cpid;
-    
+  char command[MAX_LEN_LINE];
+  char *args[] = {command, NULL};
+  int ret, status;
+  pid_t pid, cpid;
+
+  welcome();
+
     while (true) {
         char *s;
         int len;
         
-        printf("MyShell $ ");
+        shell_prompt();
         s = fgets(command, MAX_LEN_LINE, stdin);
+//        printf("%s", s);
         if (s == NULL) {
             fprintf(stderr, "fgets failed\n");
             exit(1);
         }
-        
+
         len = strlen(command);
-        printf("%d\n", len);
+//        printf("%d\n", len);
         if (command[len - 1] == '\n') {
             command[len - 1] = '\0'; 
         }
-        
-        printf("[%s]\n", command);
-      
+
+        args[0] = command;
+        if (builtin_commend(args))
+          continue;
+
+//        printf("[%s]\n", command);
         pid = fork();
         if (pid < 0) {
             fprintf(stderr, "fork failed\n");
